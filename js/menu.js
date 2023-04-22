96dfa5afc70544390702fc87fa21d69791d995e3
Miguel Arce <correo.arce@gmail.com>
2023-04-21 23:26:36 -0300

    update

---
+++ js/menu.js
@@ -0,0 +1,39 @@
+
+
+document.getElementById("item1").mouseOver = function() {
+    document.getElementById("item1").style.color = "red";
+
+}
+
+document.getElementById("item1").mouseOut = function() {
+    document.getElementById("item1").style.color = "white";
+
+}
+
+document.getElementById("item2").mouseOver = function() {
+    document.getElementById("item2").style.color = "red";
+
+}
+
+document.getElementById("item2").mouseOut = function() {
+    document.getElementById("item2").style.color = "white";
+
+}
+document.getElementById("item3").mouseOver = function() {
+    document.getElementById("item3").style.color = "red";
+
+}
+
+document.getElementById("item3").mouseOut = function() {
+    document.getElementById("item3").style.color = "white";
+
+}
+document.getElementById("item4").mouseOver = function() {
+    document.getElementById("item4").style.color = "red";
+
+}
+
+document.getElementById("item4").mouseOut = function() {
+    document.getElementById("item4").style.color = "white";
+
+}
